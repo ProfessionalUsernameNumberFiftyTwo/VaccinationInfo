5fea08f56e6a7bc40e03cd3ba7ef6a23958345a0
ZB <zivbello@gmail.com>
2022-02-01 09:35:57 -0800

    Made the app functional

--- app/src/main/java/com/example/vaccinationinfo/Covid19Service.kt
+++ app/src/main/java/com/example/vaccinationinfo/Covid19Service.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Covid19Service {
-    @GET("vaccine.coverage/countries")
+    @GET("vaccine/coverage/countries")
     fun getVaccinations(@Query("lastdays") lastDays : Int) : Call<List<Vaccination>>
 
 
