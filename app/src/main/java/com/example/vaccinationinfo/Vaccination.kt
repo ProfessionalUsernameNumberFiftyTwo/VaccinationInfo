82b83003713e226b1b082967d952269c674e6db4
ZB <zivbello@gmail.com>
2022-02-01 09:57:06 -0800

    Added options for sorting the list

--- app/src/main/java/com/example/vaccinationinfo/Vaccination.kt
+++ app/src/main/java/com/example/vaccinationinfo/Vaccination.kt
@@ -2,11 +2,12 @@ package com.example.vaccinationinfo
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.*
 
 @Parcelize
 data class Vaccination(
     val country: String,
-    val timeline: Map<String,Long>
+    val timeline: SortedMap<String,Long>
 ) : Parcelable, Comparable<Vaccination> {
     override fun compareTo(other: Vaccination): Int {
         return this.country.compareTo(other.country)
--- app/src/main/java/com/example/vaccinationinfo/VaccinationListActivity.kt
+++ app/src/main/java/com/example/vaccinationinfo/VaccinationListActivity.kt
@@ -3,6 +3,9 @@ package com.example.vaccinationinfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.vaccinationinfo.databinding.ActivityVaccinationListBinding
 import retrofit2.Call
@@ -56,9 +59,56 @@ class VaccinationListActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<Vaccination>>, t: Throwable) {
                 Log.d(TAG, "onFailure: ${t.message}")
             }
-
         })
-        
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.vaccination_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.sort_by_country_name -> {
+                sortByName()
+                adapter.notifyDataSetChanged()
+                true
+            }
+            R.id.sort_by_vaccine_count -> {
+                sortByCount()
+                adapter.notifyDataSetChanged()
+                true
+            }
+            R.id.sort_by_country_name_descending -> {
+                sortByNameDescending()
+                adapter.notifyDataSetChanged()
+                true
+            }
+            R.id.sort_by_vaccine_count_descending -> {
+                sortByCountDescending()
+                adapter.notifyDataSetChanged()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun sortByCount() {
+        adapter.dataSet = adapter.dataSet.sortedBy { it.timeline.values.last() }
+    }
+
+    private fun sortByCountDescending() {
+        adapter.dataSet = adapter.dataSet.sortedByDescending { it.timeline.values.last() }
+    }
+
+    private fun sortByName() {
+        adapter.dataSet = adapter.dataSet.sorted()
+    }
+
+    private fun sortByNameDescending() {
+        adapter.dataSet = adapter.dataSet.sortedDescending()
+    }
+
 }
